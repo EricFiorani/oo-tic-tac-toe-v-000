21a97ebed00f7a99d951e79d4399714bb2b02d37
Eric Fiorani <efiorani305@gmail.com>
2018-03-22 15:14:16 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -14,14 +14,6 @@ class TicTacToe
   [2,4,6]
 ]
 
-  def display_board
-    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
-    puts "-----------"
-    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
-    puts "-----------"
-    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
-  end
-
   def input_to_index(input)
     input.to_i-1
   end
@@ -48,6 +40,14 @@ class TicTacToe
     else
       "O"
     end
+   end
+
+   def display_board
+     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+     puts "-----------"
+     puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+     puts "-----------"
+     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
    end
 
    def turn
